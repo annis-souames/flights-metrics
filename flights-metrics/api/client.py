702ee98ab2ac0a4125224403e7f1baf32ff569bf
Annis Souames <souames.annis@gmail.com>
2023-09-25 19:56:45 +0200

    Added rate limit handling

--- flights-metrics/api/client.py
+++ flights-metrics/api/client.py
@@ -5,7 +5,9 @@ import requests
 from requests.auth import HTTPBasicAuth
 import urllib.parse
 import pdb
+import time
 from .state import StateParser
+
 
 class OpenSkyClient():
     base_url = "https://opensky-network.org/api/"
@@ -25,7 +27,8 @@ class OpenSkyClient():
             resp = requests.get(
                                 url, 
                                 params=parameters,
-                                auth = HTTPBasicAuth(self.username, self.password)
+                                auth=HTTPBasicAuth(self.username, self.password)
+                                                
                                )
         except requests.exceptions.HTTPError as errh:
             print("Http Error:", errh)
@@ -37,7 +40,20 @@ class OpenSkyClient():
             print("OOps: Something Else", err)
         else:
             print(resp.content)
+            self.updateLimitInformation(resp)
+            if resp.status_code == 429:  # In case of too many requests, wait for X seconds then retry
+                # TODO : Add logging here to inform
+                time.sleep(self.retry_after)
+                self.getResource(endpoint, parameters)
             return resp.json()
+        
+    def updateLimitInformation(self, response: requests.Response):
+        if 'X-Rate-Limit-Remaining' in response.headers:
+            self.credits = response.headers['X-Rate-Limit-Remaining']
+        if 'X-Rate-Limit-Retry-After-Seconds' in response.headers:
+            self.retry_after = response.headers['X-Rate-Limit-Retry-After-Seconds']
+        print("You have ", response.headers['X-Rate-Limit-Remaining'])
+        #print(response.headers['X-Rate-Limit-Retry-After-Seconds'])
 
     def getStatesInRegion(self, boundingBox: dict):
         # bbox = (min latitude, max latitude, min longitude, max longitude)
