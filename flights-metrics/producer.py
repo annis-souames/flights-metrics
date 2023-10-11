649b3021a5741a8606774377d509c547bfac0ce1
Annis Souames <souames.annis@gmail.com>
2023-10-11 23:30:32 +0200

    Changed main to run lambda

---
+++ flights-metrics/producer.py
@@ -0,0 +1,28 @@
+from api import FlightRadarClient
+from config import Config
+from loguru import logger
+from stream import RecordsProducer
+
+
+logger.remove()
+
+logger.add("logs/main.log", rotation="250 MB", retention="2 months")
+
+cfg = Config("env")
+kafka_cfg = Config("kafka")
+
+producer = RecordsProducer(kafka_cfg)
+
+client = FlightRadarClient()
+
+
+# Lambda function to run for producer
+def run_producer(event, context):
+    resp = client.getFlightsInRegion(cfg.get("bounding_box"))
+
+    for flight in resp:
+        try:
+            producer.send(flight)
+        except Exception as e:
+            logger.error(f"Error happened while producing a record : {str(e)}")
+            continue
