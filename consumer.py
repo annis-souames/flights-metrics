e7ab0a342ada4dc91ccad9448ea499b7eadcac0b
Annis Souames <souames.annis@gmail.com>
2023-10-15 10:33:15 +0200

    Finished S3 consumer

--- consumer.py
+++ consumer.py
@@ -3,8 +3,9 @@ from config import Config
 
 cfg = Config("env")
 kafka_cfg = Config("kafka")
+aws_cfg = Config("aws")
 
-consumer = S3Consumer(kafka_cfg)
+consumer = S3Consumer(kafka_cfg, aws_cfg)
 
 
 def run_consumer(event=None, context=None):
