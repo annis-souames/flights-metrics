1b48d9ce9ef359291e8101a1cd8e419b6a0b2c52
Annis Souames <souames.annis@gmail.com>
2023-10-15 10:33:15 +0200

    Finished S3 consumer

--- stream/S3Consumer.py
+++ stream/S3Consumer.py
@@ -22,25 +22,32 @@ class S3Consumer:
 
         self.awsAccessKeyID = awsConfig.get("aws_access_key_id")
         self.awsAccessKeySecret = awsConfig.get("aws_access_key_secret")
+        self.awsSessionToken = awsConfig.get("aws_session_token")
         self.bucket = awsConfig.get("s3")["bucket"]
         self.s3Prefix = awsConfig.get("s3")["prefix"]
 
-    def uploadToS3():
+    def uploadToS3(self, data: dict):
         s3 = boto3.client(
             "s3",
-            aws_access_key_id=AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+            aws_access_key_id=self.awsAccessKeyID,
+            aws_secret_access_key=self.awsAccessKeySecret,
+            aws_session_token=self.awsSessionToken,
         )
 
         # Assuming 'data' is a JSON object
         data_json = json.dumps(data)
 
         # Construct S3 object key based on your requirements
-        s3_object_key = f"{S3_PREFIX}flight_{data['flight_id']}.json"
+        s3_object_key = f"{self.s3Prefix}flight_{data['id']}.json"
 
-        s3.put_object(Bucket=S3_BUCKET_NAME, Key=s3_object_key, Body=data_json)
-        print(f"Uploaded data to S3: {s3_object_key}")
+        s3.put_object(Bucket=self.bucket, Key=s3_object_key, Body=data_json)
+        logger.info(f"Uploaded data to S3: {s3_object_key}")
 
     def consume(self, withS3: bool = True):
         for msg in self.consumer:
-            logger.info(msg.value)
+            try:
+                logger.info(msg.value)
+                if withS3:
+                    self.uploadToS3(msg.value)
+            except Exception as e:
+                logger.error(f"An error occured {str(e)}")
